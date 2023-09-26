570108654a1c6db88632d6d13e6782bd181687a4
hash-amit <hash.amitkumar@gmail.com>
2023-09-26 21:12:48 +0530

    Added Connection String and Model Classes

---
+++ EntityFrameWorkCodeFirstApproach/Migrations/202309261535233_mg1.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityFrameWorkCodeFirstApproach.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TblRegistrations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Pass = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TblRegistrations");
+        }
+    }
+}
---
+++ EntityFrameWorkCodeFirstApproach/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkCodeFirstApproach.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("con_string") 
+        { 
+
+        }
+
+        public DbSet<TblRegistration> TblRegistrations { get; set; }
+    }
+}
