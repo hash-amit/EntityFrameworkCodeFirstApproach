c117528174070edb840d7a2764beb5ea629da7bb
hash-amit <hash.amitkumar@gmail.com>
2023-09-26 22:08:24 +0530

    Insert using Razor

--- EntityFrameWorkCodeFirstApproach/App_Start/RouteConfig.cs
+++ EntityFrameWorkCodeFirstApproach/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EntityFrameWorkCodeFirstApproach
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Reg", action = "AddUser", id = UrlParameter.Optional }
             );
         }
     }
---
+++ EntityFrameWorkCodeFirstApproach/Controllers/RegController.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameWorkCodeFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWorkCodeFirstApproach.Controllers
+{
+    public class RegController : Controller
+    {
+        DatabaseContext dbc = new DatabaseContext();
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult AddUser(TblRegistration _tr)
+        {
+            dbc.TblRegistrations.Add(_tr);
+            dbc.SaveChanges();
+            return View();
+        }
+
+
+    }
+}
